2e1a43463139ff78199ce428efaed1e03a4116cf
mehdi <mehdi.dellegi@esprit.tn>
2023-06-08 15:07:20 -0700

    new update

--- src/main/java/com/supportportalMehdi/demo/AAAA/PARSING/controller/SocieteController.java
+++ src/main/java/com/supportportalMehdi/demo/AAAA/PARSING/controller/SocieteController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SocieteController implements SocieteApi {
 
@@ -23,5 +25,10 @@ public class SocieteController implements SocieteApi {
     @Override
     public ResponseEntity<SocieteDto> createSociete(SocieteDto societeDto) {
         return ResponseEntity.ok(societeService.createSociete(societeDto));
+    }
+
+    @Override
+    public ResponseEntity<List<SocieteDto>> getAllSociete() {
+        return ResponseEntity.ok(societeService.getAllSocietes());
     }
 }
--- src/main/java/com/supportportalMehdi/demo/AAAA/PARSING/controller/api/SocieteApi.java
+++ src/main/java/com/supportportalMehdi/demo/AAAA/PARSING/controller/api/SocieteApi.java
@@ -4,12 +4,17 @@ package com.supportportalMehdi.demo.AAAA.PARSING.controller.api;
 import com.supportportalMehdi.demo.AAAA.PARSING.dto.SocieteDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 import static com.supportportalMehdi.demo.AAAA.PARSING.utils.Constants.APP_ROOT;
 
 public interface SocieteApi {
     @PostMapping(value=APP_ROOT+"/societe/create",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<SocieteDto> createSociete(@RequestBody SocieteDto societeDto);
+    @GetMapping(value=APP_ROOT+"/societe/all",produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<List<SocieteDto>> getAllSociete();
 }
